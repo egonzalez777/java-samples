5af9ad283aa24369d6b5d85111809c441c4aec28
Eric Gonzalez <eric.gonz.gomez@gmail.com>
2015-02-11 10:06:15 -0800

    Added FixedStackOfStrings.java.

---
+++ FixedStackOfStrings.java
@@ -0,0 +1,41 @@
+public class FixedStackOfStrings {
+    /**
+     * Array Stack Implementation
+     * @param int capacity
+     */
+
+    private String[] s;
+    private int N = 0;
+
+    /**
+     * @param int capacity
+     *
+     */
+    public FixedStackOfStrings(int capacity) {
+        s = new String[capacity];
+    }
+
+    /**
+     * @param void
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    /**
+     * @param String item
+     * @return void
+     */
+    public void push(String item) {
+        s[N++] = item;
+    }
+
+    /**
+     * @param void
+     * @return String
+     */
+    public String pop() {
+        return s[--N];
+    }
+}
